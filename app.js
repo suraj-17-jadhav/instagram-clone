46ab862133b749015dd26ba4af166e78eacff310
suraj jadhav <surajadhav0149@gmail.com>
2023-10-17 23:38:19 +0530

    first-commit

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ mongoose.connection.on("error",()=>{
 })
 
 // serving the frontend
-app.use(express.static(path.join(__dirname, "./frontend/build")))
+app.use(express.static(path.join(__dirname, "./frontend/build/index.html")))
 app.get("*",(req,res)=>{
    res.sendFile(
       path.join(__dirname,"./frontend/build/index.html"),
